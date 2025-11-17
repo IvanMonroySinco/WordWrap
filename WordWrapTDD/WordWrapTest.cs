137413ad007472a7d45e329f8ce297b157c08159
Ivan Rene Monroy Santos <ivan.monroy@sinco.com.co>
2025-11-17 17:09:29 -0500

    🟥Dado_StringPezYNumeroDeColumnas5_Debe_RetornarCadenaSinCortar

---
+++ WordWrapTDD/WordWrapTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+namespace WordWrapTDD;
+
+public class WordWrapTest
+{
+    [Fact]
+    public void Dado_StringPezYNumeroDeColumnas5_Debe_RetornarCadenaSinCortar()
+    {
+        var wordWrap = new WordWrap();
+
+        var resultado = wordWrap.AjustarTexto("Pez", 5);
+
+        resultado.Should().Be("Pez");
+
+    }
+}
+
+public class WordWrap
+{
+    public string AjustarTexto(string pez, int i)
+    {
+        throw new NotImplementedException();
+    }
+}
